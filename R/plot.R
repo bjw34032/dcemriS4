4d7bae34b8d54d007c135f38ecb4c5a4dccacffb
bjw34032 <bjw34032@79b81491-4cf2-4846-a328-77de2410c9e9>
2009-08-13 10:19:49 +0000

    Update to plotting routines

--- R/plot.R
+++ R/plot.R
@@ -1,3 +1,37 @@
+##
+##
+## Copyright (c) 2009, Brandon Whitcher and Volker Schmid
+## All rights reserved.
+## 
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are
+## met:
+## 
+##     * Redistributions of source code must retain the above copyright
+##       notice, this list of conditions and the following disclaimer. 
+##     * Redistributions in binary form must reproduce the above
+##       copyright notice, this list of conditions and the following
+##       disclaimer in the documentation and/or other materials provided
+##       with the distribution.
+##     * The names of the authors may not be used to endorse or promote
+##       products derived from this software without specific prior
+##       written permission.
+## 
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+## HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+## 
+## $ Id: $
+##
+
 #############################################################################
 ## image() for class="nifti"
 #############################################################################
@@ -6,42 +40,42 @@ setMethod("image", signature(x="nifti"),
           function(x, z=1, w=1, col=gray(0:64/64),
                    plot.type=c("multiple","single"), zlim=NULL,
                    xlab="", ylab="", axes=FALSE, ...) {
+            ## set dimensions
             X <- nrow(x) ; Y <- ncol(x) ; Z <- dim(x)[3] ; W <- dim(x)[4]
+            ## check dimensions
             if (X == 0 || Y == 0 || Z == 0)
               stop("size of NIfTI volume is zero, nothing to plot")
+            ## check for z-limits; use internal by default
             if (is.null(zlim))
               zlim <- c(x@"cal_min", x@"cal_max")
-            if (is.na(W)) {
-              ## three-dimensional array
+            ## single or multiple images?
+            if (plot.type[1] == "multiple")
+              index <- 1:Z
+            else
+              index <- z
+            lz <- length(index)
+            ## plotting
+            if (is.na(W)) { # three-dimensional array
               if (z < 1 || z > Z)
                 stop("slice \"z\" out of range")
-              oldpar <- par()
-              if (plot.type[1] == "multiple")
-                index <- 1:Z
-              else
-                index <- z
-              lz <- length(index)
+              oldpar <- par(no.readonly=TRUE)
               par(mfrow=ceiling(rep(sqrt(lz),2)), mar=rep(0,4))
               for (z in index)
                 graphics:::image(1:X, 1:Y, x[,,z], col=col, zlim=zlim,
                                  axes=axes, xlab=xlab, ylab=ylab, ...)
-            } else {
-              ## four-dimensional array
+            } else { # four-dimensional array
               if (w < 1 || w > W)
                 stop("volume \"w\" out of range")
               if (z < 1 || z > Z)
                 stop("slice \"z\" out of range")
-              if (plot.type[1] == "multiple")
-                index <- 1:Z
-              else
-                index <- z
-              lz <- length(index)
+              oldpar <- par(no.readonly=TRUE)
               par(mfrow=ceiling(rep(sqrt(lz),2)), mar=rep(0,4))
               for (z in index)
                 graphics:::image(1:X, 1:Y, x[,,z,w], col=col, zlim=zlim,
                                  axes=axes, xlab=xlab, ylab=ylab, ...)
             }
-            
+            par(oldpar)
+            invisible()
           })
 
 #############################################################################
@@ -53,62 +87,49 @@ setMethod("overlay", signature(x="nifti", y="nifti"),
           function(x, y, z=1, w=1, col.x=gray(0:64/64), col.y=hotmetal(),
                    zlim.x=NULL, zlim.y=NULL, plot.type=c("multiple","single"),
                    xlab="", ylab="", axes=FALSE, ...) {
-            ## should do plot.type=c("multiple","single") like plot.ts
+            ## both volumes must have the same dimension
             if (!all(dim(x)[1:3] == dim(y)[1:3]))
               stop("dimensions of \"x\" and \"y\" must be equal")
+            ## set dimensions
             X <- nrow(x) ; Y <- ncol(x) ; Z <- dim(x)[3] ; W <- dim(x)[4]
+            ## check dimensions
             if (X == 0 || Y == 0 || Z == 0)
               stop("size of NIfTI volume is zero, nothing to plot")
+            ## check for z-limits in x; use internal by default
             if (is.null(zlim.x))
               zlim.x <- c(x@"cal_min", x@"cal_max")
+            ## check for z-limits in y; use internal by default
             if (is.null(zlim.y))
               zlim.y <- c(y@"cal_min", y@"cal_max")
-            if (is.na(W)) {
-              ## three-dimensional array
-              oldpar <- par()
-              if (plot.type[1] == "single") {
-                if (z < 1 || z > Z)
-                  stop("slice \"z\" out of range")
-                par(mfrow=c(1,1), mar=rep(0,4))
+            if (plot.type[1] == "multiple")
+              index <- 1:Z
+            else
+              index <- z
+            lz <- length(index)
+            if (is.na(W)) { # three-dimensional array
+              if (z < 1 || z > Z)
+                stop("slice \"z\" out of range")
+              oldpar <- par(no.readonly=TRUE)
+              par(mfrow=ceiling(rep(sqrt(lz),2)), mar=rep(0,4))
+              for (z in index) {
                 graphics:::image(1:X, 1:Y, x[,,z], col=col.x, zlim=zlim.x,
                                  axes=axes, xlab=xlab, ylab=ylab, ...)
                 graphics:::image(1:X, 1:Y, y[,,z], col=col.y, zlim=zlim.y,
                                  add=TRUE)
-              } else {
-                par(mfrow=c(ceiling(sqrt(Z)), ceiling(sqrt(Z))), mar=rep(0,4))
-                for (z in 1:Z) {
-                  graphics:::image(1:X, 1:Y, x[,,z], col=col.x, zlim=zlim.x,
-                                   axes=axes, xlab=xlab, ylab=ylab, ...)
-                  graphics:::image(1:X, 1:Y, y[,,z], col=col.y, zlim=zlim.y,
-                                   add=TRUE)
-                }
               }
-            } else {
-              ## four-dimensional array
-              oldpar <- par()
+            } else { # four-dimensional array
               if (w < 1 || w > W)
                 stop("volume \"w\" out of range")
               if (z < 1 || z > Z)
                 stop("slice \"z\" out of range")
-              if (plot.type == "single") {
-                par(mfrow=c(1,1), mar=rep(0,4))
+              oldpar <- par(no.readonly=TRUE)
+              par(mfrow=ceiling(rep(sqrt(lz),2)), mar=rep(0,4))
+              for (z in index) {
                 graphics:::image(1:X, 1:Y, x[,,z,w], col=col.x, zlim=zlim.x,
                                  axes=axes, xlab=xlab, ylab=ylab, ...)
                 graphics:::image(1:X, 1:Y, y[,,z], col=col.y, zlim=zlim.y,
                                  add=TRUE)
-              } else {
-                par(mfrow=c(ceiling(sqrt(Z)), ceiling(sqrt(Z))), mar=rep(0,4))
-                for (z in 1:Z) {
-                  graphics:::image(1:X, 1:Y, x[,,z,w], col=col.x, zlim=zlim.x,
-                                   axes=axes, xlab=xlab, ylab=ylab, ...)
-                  graphics:::image(1:X, 1:Y, y[,,z], col=col.y, zlim=zlim.y,
-                                   add=TRUE)
-                }
               }
-              graphics:::image(1:X, 1:Y, x[,,z,w], col=colx, zlim=zlimx,
-                               axes=axes, xlab=xlab, ylab=ylab, ...)
-              graphics:::image(1:X, 1:Y, y[,,z], col=coly, zlim=zlimy,
-                               add=TRUE)
             }
             par(oldpar)
             invisible()
@@ -130,8 +151,7 @@ setMethod("orthographic", signature(x="nifti"),
               stop("size of NIfTI volume is zero, nothing to plot")
             if (is.null(zlim))
               zlim <- c(x@"cal_min", x@"cal_max")
-            if (is.na(W)) {
-              ## three-dimensional array
+            if (is.na(W)) { # three-dimensional array
               oldpar <- par()
               par(mfrow=c(2,2), mar=rep(0,4))
               graphics::image(1:X, 1:Z, x[,xyz[2],], col=col,
@@ -149,8 +169,7 @@ setMethod("orthographic", signature(x="nifti"),
                               xlab=xlab, ylab=ylab, axes=axes, ...)
               if (crosshairs)
                 abline(h=xyz[2], v=xyz[1], col=col.crosshairs)
-            } else {
-              ## four-dimensional array
+            } else { # four-dimensional array
               if (w < 1 || w > W)
                 stop("volume \"w\" out of range")
               oldpar <- par()
