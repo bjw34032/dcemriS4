6c97b6b60bfe72eedd41732bd72e569ffed7cb7c
zeripath <zeripath@79b81491-4cf2-4846-a328-77de2410c9e9>
2009-08-17 13:21:06 +0000

    Add wrapper for bayes

--- R/wrappers.R
+++ R/wrappers.R
@@ -1,17 +1,25 @@
-dcemri.lm.s4 <- function(nifticonc, time, mask, model="extended", aif=NULL,
-    nprint=0, user=NULL, verbose=FALSE, ...) {
+dcemri.lm.s4 <- function(nifti, time, mask, ...) {
   ## input:
   ##	    nifticonc: a niftis4 object of Gd concentrations
   ##	    time: timepoints of aquisition
   ##	    img.mask: array of voxels to fit
   
 
-  lm.fit <- dcemri.lm(nifticonc, time, mask, model=model, aif=aif, nprint=nprint, user=user, verbose=verbose, ...)
-  
-  time <- lm.fit$time
-  lm.fit$time <- NULL
+  lm.fit <- dcemri.lm(nifti, time, mask, ...)
+  fit.nifti(lm.fit, nifti)
+}
 
-  s4.fit <- lapply(lm.fit, function(x) { as.nifti(x, nifticonc) })
+dcemri.bayes.s4 <- function(nifti, time, img.mask, ...) {
+  bayes.fit <- dcemri.bayes(nifti, time, img.mask, ...) 
+  fit.nifti(bayes.fit, nifti)
+}
+
+fit.nifti <- function(fit, nifti) {
+  time <- fit$time
+  fit$time <- NULL
+
+  s4.fit <- lapply(fit, function(x) { as.nifti(x, nifti) })
+  
   lapply(names(s4.fit), function(x) {
 	niftiobject <- s4.fit[[x]]
 	niftiobject@"intent_code" <- nifit.intent.code[["Estimate"]]
