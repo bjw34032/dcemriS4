0b3501d8eb70063b0a7eb6f44909e99be255d345
zeripath <zeripath@79b81491-4cf2-4846-a328-77de2410c9e9>
2009-08-19 10:58:38 +0000

    Added dependency to dcemri and making dcemri.etc generic

--- R/wrappers.R
+++ R/wrappers.R
@@ -1,11 +1,40 @@
-dcemri.lm.s4 <- function(nim, time, mask, ...) {
-  as.nifti(dcemri.lm(nim, time, mask, ...), nim)
+dcemri.lm.s4 <- function(conc, time, mask, ...) {
+  as.nifti(dcemri.lm(conc, time, mask, ...), conc)
 }
 
-dcemri.bayes.s4 <- function(nim, time, img.mask, ...) {
-  as.nifti(dcemri.bayes(nim, time, img.mask, ...), nim)
+dcemri.bayes.s4 <- function(conc, time, img.mask, ...) {
+  as.nifti(dcemri.bayes(conc, time, img.mask, ...), conc)
 }
 
-dcemri.spline.s4 <- function(nim, time, img.mask, ...) {
-  as.nifti(dcemri.spline(nim, time, img.mask, ...), nim)
+dcemri.spline.s4 <- function(conc, time, img.mask, ...) {
+  as.nifti(dcemri.spline(conc, time, img.mask, ...), conc)
 }
+
+setGeneric("dcemri.lm")
+setMethod("dcemri.lm", signature(conc="nifti"), dcemri.lm.s4)
+setMethod("dcemri.lm", signature(conc="array"), function(conc,...) { 
+      dcemri.lm.s4(as("nifti", conc), ...)
+    })
+setMethod("dcemri.lm", signature(conc="anlz"), function(conc,...) { 
+      dcemri.lm.s4(as("nifti", conc), ...)
+    })
+
+setGeneric("dcemri.bayes")
+setMethod("dcemri.bayes", signature(conc="nifti"), dcemri.bayes.s4)
+setMethod("dcemri.bayes", signature(conc="array"), function(conc,...) { 
+      dcemri.bayes.s4(as("nifti", conc), ...)
+    })
+setMethod("dcemri.bayes", signature(conc="anlz"), function(conc,...) { 
+      dcemri.bayes.s4(as("nifti", conc), ...)
+    })
+
+setGeneric("dcemri.spline")
+setMethod("dcemri.spline", signature(conc="nifti"), dcemri.spline.s4)
+setMethod("dcemri.spline", signature(conc="array"), function(conc,...) { 
+      dcemri.spline.s4(as("nifti", conc), ...)
+    })
+setMethod("dcemri.spline", signature(conc="anlz"), function(conc,...) { 
+      dcemri.spline.s4(as("nifti", conc), ...)
+    })
+
+
