1a2fd6e0453a878c3ba8e89181e6adf835c94700
bjw34032 <bjw34032@79b81491-4cf2-4846-a328-77de2410c9e9>
2009-08-19 12:29:26 +0000

    Added BSD license

--- R/convert_nifti.R
+++ R/convert_nifti.R
@@ -1,3 +1,36 @@
+##
+##
+## Copyright (c) 2009, Brandon Whitcher and Volker Schmid
+## All rights reserved.
+## 
+## Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are
+## met:
+## 
+##     * Redistributions of source code must retain the above copyright
+##       notice, this list of conditions and the following disclaimer. 
+##     * Redistributions in binary form must reproduce the above
+##       copyright notice, this list of conditions and the following
+##       disclaimer in the documentation and/or other materials provided
+##       with the distribution.
+##     * The names of the authors may not be used to endorse or promote
+##       products derived from this software without specific prior
+##       written permission.
+## 
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+## HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+## 
+## $ Id: $
+##
 
 nifti.datatypes <- list(UINT8 = 2,
                         INT16 = 4,
@@ -36,42 +69,43 @@ nifti.datatypes.bitsperpix <- list(UINT8 = 8,
 convert.datatype <- function(datatype)
   names(which(nifti.datatypes == datatype))
 
-nifti.intent.code <- list(
-         "None" = 0,
-         "Correl" = 2,
-         "Ttest" = 3,
-         "Ftest" = 4,
-         "Zscore" = 5,
-         "Chisq" = 6,
-         "Beta" = 7,
-         "Binom" = 8,
-         "Gamma" = 9,
-         "Poisson" = 10,
-         "Normal" = 11,
-         "Ftest_Nonc" = 12,
-         "Chisq_Nonc" = 13,
-         "Logistic" = 14,
-         "Laplace" = 15,
-         "Uniform" = 16,
-         "Ttest_Nonc" = 17,
-         "Weibull" = 18,
-         "Chi" = 19,
-         "Invgauss" = 20,
-         "Extval" = 21,
-         "Pval" = 22,
-         "Logpval" = 23,
-         "Log10pval" = 24,
-         "Estimate" = 1001,      # estimate of some parameter
-         "Label" = 1002,         # index into some set of labels
-         "Neuroname" = 1003,     # index into the NeuroNames labels set
-         "Genmatrix" = 1004,     # M x N matrix at each voxel
-         "Symmatrix" = 1005,     # N x N symmetric matrix at each voxel
-         "Dispvect" = 1006,      # a displacement field
-         "Vector" = 1007,        # a displacement vector
-         "Pointset" = 1008,      # a spatial coordinate
-         "Triangle" = 1009,      # triple of indexes
-         "Quaternion" = 1010,    # a quaternion
-         "Dimless" = 1011)       # Dimensionless value - no params
+nifti.intent.code <- list("None" = 0,
+                          "Correl" = 2,
+                          "Ttest" = 3,
+                          "Ftest" = 4,
+                          "Zscore" = 5,
+                          "Chisq" = 6,
+                          "Beta" = 7,
+                          "Binom" = 8,
+                          "Gamma" = 9,
+                          "Poisson" = 10,
+                          "Normal" = 11,
+                          "Ftest_Nonc" = 12,
+                          "Chisq_Nonc" = 13,
+                          "Logistic" = 14,
+                          "Laplace" = 15,
+                          "Uniform" = 16,
+                          "Ttest_Nonc" = 17,
+                          "Weibull" = 18,
+                          "Chi" = 19,
+                          "Invgauss" = 20,
+                          "Extval" = 21,
+                          "Pval" = 22,
+                          "Logpval" = 23,
+                          "Log10pval" = 24,
+                          "Estimate" = 1001,  # estimate of some parameter
+                          "Label" = 1002,     # index into some set of labels
+                          "Neuroname" = 1003, # index into the NeuroNames
+                                              # labels set
+                          "Genmatrix" = 1004, # M x N matrix at each voxel
+                          "Symmatrix" = 1005, # N x N symmetric matrix at
+                                              # each voxel
+                          "Dispvect" = 1006,  # a displacement field
+                          "Vector" = 1007,    # a displacement vector
+                          "Pointset" = 1008,  # a spatial coordinate
+                          "Triangle" = 1009,  # triple of indexes
+                          "Quaternion" = 1010, # a quaternion
+                          "Dimless" = 1011)   # Dimensionless value - no params
 
 convert.intent <- function(intent.code)
   names(which(nifti.intent.code == intent.code))
@@ -212,9 +246,18 @@ as.nifti <- function(from, value=NULL, verbose=FALSE) {
   return(nim)
 }
 
+############################################################################
+## as("array", "nifti")
+############################################################################
+
 setAs("array", "nifti",
       function(from) { as.nifti(from) },
       function(from, value) { as.nifti(from, value) } )
+
+############################################################################
+## as("list", "nifti")
+############################################################################
+
 setAs("list", "nifti",
       function(from) { as.nifti(from) },
       function(from, value) { as.nifti(from, value) } )
