3aa3d5a1948a99619a002b6bc45089e6cd03919c
bjw34032 <bjw34032@79b81491-4cf2-4846-a328-77de2410c9e9>
2009-09-04 15:23:33 +0000

    Added Analyze data to /inst/nifti and more demos

---
+++ demo/avg152T1.R
@@ -0,0 +1,3 @@
+avg152T1 <- readANALYZE(system.file("nifti/avg152T1.img.gz",
+                                    package="dcemriS4"))
+image(avg152T1)
---
+++ demo/avg152T1RL.R
@@ -0,0 +1,3 @@
+mniRL <- readNIfTI(system.file("nifti/avg152T1_RL_nifti.nii.gz",
+                               package="dcemriS4"))
+image(mniRL)
